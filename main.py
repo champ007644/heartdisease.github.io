e82a9994867e5ca1de3345367053e966cf23c7ac
champ007644 <kushagrasingh6355@gmail.com>
2023-04-11 10:59:04 +0530

    my heart disease web app

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+
+model = pickle.load(open('model1.pk1','rb'))
+app = Flask(__name__)
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    age = request.form.get('age')
+    sex = request.form.get('sex')
+    cp = request.form.get('cp')
+    trestbps = request.form.get('trestbps')
+    chol = request.form.get('chol')
+    fbs = request.form.get('fbs')
+    restecg = request.form.get('restecg')
+    thalach = request.form.get('thalach')
+    exang = request.form.get('exang')
+    oldpeak = request.form.get('oldpeak')
+    slope = request.form.get('slope')
+    ca = request.form.get('ca')
+    thal = request.form.get('thal')
+
+
+
+    input_query = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]],dtype=float)
+    result = model.predict(input_query)[0]
+
+
+    return jsonify({'target':str(result)})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
